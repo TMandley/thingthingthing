e96c0a143b25f757956c3f54f3d77f717d2c7ecd
TMandley <51174167+TMandley@users.noreply.github.com>
2021-12-03 14:31:17 -0700

    Update tester.js

--- tester.js
+++ tester.js
@@ -4,21 +4,17 @@ jQuery(document).ready( function (){
     let myListy = [];
     $('table.ellipsis-wrap tbody tr td:first-of-type a').each(function() {
         myListy.push($(this).text());
-    });
-
-    // checks list of products from added html against original product list. 
-    // if names match, sets quantity to new quantity
-    $('table.waffle tbody tr').each(function() {
-        console.log('first line');
-        if($(this).children().length > 0){
-            console.log('in this');
-            let searchName = $(':nth-child(2)', this).text();
-            let listIndex = myListy.indexOf(searchName);
-            // must add one for nth child call
-            //if value is in list, set quantity via attr change
-            if(listIndex !== -1){
-                $(`table.ellipsis-wrap tbody tr:nth-child(${listIndex + 1}) td input.onhand`).attr('value', $(':nth-child(3)', this).text());
+    }, function() {
+        $('table.waffle tbody tr').each(function() {
+            if($(this).children().length > 0){
+                let searchName = $(':nth-child(2)', this).text();
+                let listIndex = myListy.indexOf(searchName);
+                //if value is in list, set quantity via attr change
+                if(listIndex !== -1){
+                    $(`table.ellipsis-wrap tbody tr:nth-child(${listIndex + 1}) td input.onhand`).attr('value', $(':nth-child(3)', this).text());
+                };
             };
-        };
+        });
     });
+    console.log(myListy);
 });
